58281acd97dd4cc855d9e7d771366704541a964d
hardikjain0810 <hj791983@gmail.com>
2026-03-30 21:00:59 +0530

    Updated

--- api/user_profile.py
+++ api/user_profile.py
@@ -124,7 +124,7 @@ def get_student_sessions(
         ).filter(
             Booking.student_id == request.student_id
         ).order_by(
-            Booking.start_at.asc()
+            Booking.starts_at.asc()
         ).all()
 
         session_list = []
